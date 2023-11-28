0a64bab6127ba5961b53685e32e68a471be85a0c
NHT <hoangtuan910@gmail.com>
2023-11-28 21:45:28 +0700

    add dto to food recommendation request

--- src/domain/web-customer/controller/web.customer.food.controller.ts
+++ src/domain/web-customer/controller/web.customer.food.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { WebCustomerFoodService } from '../service/web.customer.food.service';
+import { FoodRecommendationRequest } from '../dto/food-recommendation-request.dto';
 
 @Controller('web-customer/food')
 export class WebCustomerFoodController {
   constructor(private readonly foodService: WebCustomerFoodService) {}
 
   @Get('get-general-food-recomendation')
-  async getGeneralFoodRecomendation(): Promise<any> {
-    console.log('run getGeneralFoodRecomendation');
-    return this.foodService.getGeneralFoodRecomendation();
+  async getGeneralFoodRecomendation(
+    @Body() foodRequest: FoodRecommendationRequest,
+  ): Promise<any> {
+    return this.foodService.getGeneralFoodRecomendation(foodRequest);
   }
 }
--- src/domain/web-customer/service/web.customer.food.service.ts
+++ src/domain/web-customer/service/web.customer.food.service.ts
@@ -1,13 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { FoodRecommendationRequest } from '../dto/food-recommendation-request.dto';
 
 @Injectable()
 export class WebCustomerFoodService {
   @Inject('RESTAURANT_SERVICE') private readonly restaurantClient: ClientProxy;
-  async getGeneralFoodRecomendation(): Promise<any> {
+  async getGeneralFoodRecomendation(
+    data: FoodRecommendationRequest,
+  ): Promise<any> {
     return await firstValueFrom(
-      this.restaurantClient.send({ cmd: 'get_general_food_recomendation' }, {}),
+      this.restaurantClient.send(
+        { cmd: 'get_general_food_recomendation' },
+        data,
+      ),
     );
   }
 }
