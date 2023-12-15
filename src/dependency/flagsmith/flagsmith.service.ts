0ec8213751fe6f92d1c1ebb216d02c5b56d70acb
NHT <hoangtuan910@gmail.com>
2023-12-15 15:51:36 +0700

    add isFeatureEnabled for flag module

--- src/dependency/flagsmith/flagsmith.service.ts
+++ src/dependency/flagsmith/flagsmith.service.ts
@@ -11,11 +11,12 @@ export class FlagsmitService {
   ) {}
   private flagsmith: Flagsmith;
   private flags: any;
-  private identifier: string = '';
+  // private identifier: string = '';
 
   async init() {
+    const flagSmithKey = this.configService.get<string>('flagSmithKey');
     this.flagsmith = new Flagsmith({
-      environmentKey: this.configService.get<string>('flagSmithKey'),
+      environmentKey: flagSmithKey,
     });
     this.flags = await this.flagsmith.getEnvironmentFlags();
 
@@ -24,33 +25,45 @@ export class FlagsmitService {
   }
 
   private emitAllMicroServices() {
-    this.restaurantClient.emit('refresh_flags', this.identifier);
+    // this.restaurantClient.emit('refresh_flags', this.identifier);
+    this.restaurantClient.emit('refresh_flags', {});
   }
 
   public async refreshFlags() {
     let mess = '';
-    if (this.identifier == '') {
-      this.flags = await this.flagsmith.getEnvironmentFlags();
-      mess = 'Get the environment flags successfully';
-    } else if (this.identifier != '') {
-      this.flags = await this.flagsmith.getIdentityFlags(this.identifier);
-      mess = 'Get the indentity flags successfully';
-    }
+    // if (this.identifier == '') {
+    //   this.flags = await this.flagsmith.getEnvironmentFlags();
+    //   mess = 'Get the environment flags successfully';
+    // } else if (this.identifier != '') {
+    //   this.flags = await this.flagsmith.getIdentityFlags(this.identifier);
+    //   mess = 'Get the indentity flags successfully';
+    // }
+
+    this.flags = await this.flagsmith.getEnvironmentFlags();
+    mess = 'Get the environment flags successfully';
     //Edit all of the micro-services
     this.emitAllMicroServices();
     return mess;
   }
-  public async activateIdentity(identifier: string) {
-    this.identifier = identifier;
-    await this.refreshFlags();
-    return 'Activate the identifier successfully';
-  }
-  public async deactivateIdentity() {
-    this.identifier = '';
-    await this.refreshFlags();
-    return 'Deactivate the identifier successfully';
-  }
+  // public async activateIdentity(identifier: string) {
+  //   this.identifier = identifier;
+  //   await this.refreshFlags();
+  //   return 'Activate the identifier successfully';
+  // }
+  // public async deactivateIdentity() {
+  //   this.identifier = '';
+  //   await this.refreshFlags();
+  //   return 'Deactivate the identifier successfully';
+  // }
   public getFlags() {
     return this.flags;
+  }
+
+  public isFeatureEnabled(featureName: string): boolean {
+    const configFeatureName = this.configService.get<string>('featureFlag');
+    if (configFeatureName != '' && configFeatureName == featureName) {
+      return true;
+    }
+    return this.flags.isFeatureEnabled(featureName);
   }
 }
