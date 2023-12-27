7e7fd132e108625381c61da7fced12991ebd77c0
NHT <hoangtuan910@gmail.com>
2023-12-27 13:18:45 +0700

    add the http status check

--- src/domain/web-customer/controller/web.customer.restaurant.controller.ts
+++ src/domain/web-customer/controller/web.customer.restaurant.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Inject } from '@nestjs/common';
 import { WebCustomerRestaurantService } from '../service/web.customer.restaurant.service';
 import { RestaurantRecommendationRequest } from '../dto/restaurant-recommendation-request.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { FlagsmitService } from 'src/dependency/flagsmith/flagsmith.service';
 
 @ApiTags('Web customer restaurant')
 @Controller('web-customer/restaurant')
 export class WebCustomerRestaurantController {
   constructor(
     private readonly restaurantService: WebCustomerRestaurantService,
+    @Inject('FLAGSMITH_SERVICE') private readonly flagService: FlagsmitService,
   ) {}
 
   @Get('get-general-recomendation')
   async getGeneralRestaurantRecomendation(
     @Body() requestData: RestaurantRecommendationRequest,
   ): Promise<any> {
+    if (
+      this.flagService.isFeatureEnabled(
+        'fes-19-refactor-all-the-end-point-with-general-response',
+      )
+    ) {
+      const res =
+        await this.restaurantService.getGeneralRestaurantRecomendation(
+          requestData,
+        );
+      if (res.statusCode >= 400) {
+        throw new HttpException(res, res.statusCode);
+      }
+      return res;
+    }
+
+    //CURRENT LOGIC
     return this.restaurantService.getGeneralRestaurantRecomendation(
       requestData,
     );
