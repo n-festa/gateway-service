919e70a2b12761baae72dcd520276ff5df2a883d
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-01-04 13:25:12 +0700

    FES-20-fix-cors-issue (#16)
    * config CORS option
    
    * change port of FE
    
    ---------
    
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,11 @@ async function bootstrap() {
 
   await setupSwagger(app);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   console.log(`---- APP PORT ${configService.get<number>('appPort')}`);
   await app.listen(configService.get<number>('appPort') || DEFAULT_PORT);
 }
