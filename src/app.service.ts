3b53bb4f906228b1084a1eec74e76dcc9eb17559
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-01-21 22:08:56 +0700

    test cicd (#38)
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! - Test CI 2';
+    return 'Hello from 2ALL API';
   }
 }
