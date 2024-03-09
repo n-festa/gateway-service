8c500e4f3d853f3f9208eaa9a9ede49e9a24b446
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-03-09 15:52:31 +0700

    FES-78-apply-new-cut-off-time-concept (#67)
    * adjust dtos
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/domain/web-customer/dto/food-recommendation-request.dto.ts
+++ src/domain/web-customer/dto/food-recommendation-request.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
 import { FetchMode } from 'src/enum';
 
 export class FoodRecommendationRequest {
+  @ApiProperty()
   lat: number;
+  @ApiProperty()
   long: number;
+  @ApiProperty()
+  @IsEnum(FetchMode)
   fetch_mode: FetchMode;
 }
