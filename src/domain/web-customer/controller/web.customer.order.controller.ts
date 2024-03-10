67c38537b7f8c59e886a47c74138c6fbb756d7e0
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-03-10 21:47:13 +0700

    FES-56-get-payment-method (#69)
    * add logic and apply public decorator
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/domain/web-customer/controller/web.customer.order.controller.ts
+++ src/domain/web-customer/controller/web.customer.order.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpException,
   Post,
@@ -14,8 +15,10 @@ import { Roles } from 'src/decorator/roles.decorator';
 import { GetApplicationFeeRequest } from '../dto/get-application-fee-request.dto';
 import { Role } from 'src/enum';
 import { GetApplicationFeeResponse } from '../dto/get-application-fee-response.dto';
+import { GetPaymentMethodResponse } from '../dto/get-payment-method-response.dto';
+import { Public } from 'src/decorator/public.decorator';
 
-@ApiTags(' Cart')
+@ApiTags('Order')
 @UseGuards(AccessTokenGuard, RolesGuard)
 @Controller('web-customer/order')
 @Controller()
@@ -30,6 +33,20 @@ export class WebCustomerOrderController {
   ): Promise<GetApplicationFeeResponse> {
     try {
       const res = await this.orderService.getApplicationFee(requestData);
+      return res;
+    } catch (error) {
+      if (error?.error_code) {
+        throw new HttpException(error, 400);
+      } else {
+        throw new HttpException(error, 500);
+      }
+    }
+  }
+  @Get('get-payment-method')
+  @Public()
+  async getPaymentMethod(): Promise<GetPaymentMethodResponse> {
+    try {
+      const res = await this.orderService.getPaymentMethod();
       return res;
     } catch (error) {
       if (error?.error_code) {
