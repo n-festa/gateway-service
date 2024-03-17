bf0d48e23478303741f3df6ff4319fcfd1582fe5
LiamNGuyen <lamnv@guide.inc>
2024-03-17 16:06:24 +0700

    Fix: moving api to web-customer and add global handler

--- src/dependency/ahamove/ahamove.module.ts
+++ src/dependency/ahamove/ahamove.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AhamoveController } from './ahamove.controller';
 import { AhamoveService } from './ahamove.service';
 import { WebCustomerModule } from 'src/domain/web-customer/web-customer.module';
@@ -6,6 +6,7 @@ import { WebCustomerModule } from 'src/domain/web-customer/web-customer.module';
 @Module({
   controllers: [AhamoveController],
   providers: [AhamoveService],
-  imports: [WebCustomerModule],
+  imports: [forwardRef(() => WebCustomerModule)],
+  exports: [AhamoveService],
 })
 export class AhamoveModule {}
