b6c305aa508452a28c7aaeec2adb86faa9138f1b
NHT <hoangtuan910@gmail.com>
2024-03-31 14:55:42 +0700

    add fetch_mode for get food for restaurant

--- src/domain/web-customer/dto/get-available-food-by-restaurant-request.dto.ts
+++ src/domain/web-customer/dto/get-available-food-by-restaurant-request.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { FetchMode } from 'src/enum';
 
 export class GetAvailableFoodByRestaurantRequest {
   @ApiProperty()
   menu_item_id: number;
+  @ApiProperty()
+  @IsEnum(FetchMode)
+  @IsOptional()
+  fetch_mode: FetchMode;
 }
